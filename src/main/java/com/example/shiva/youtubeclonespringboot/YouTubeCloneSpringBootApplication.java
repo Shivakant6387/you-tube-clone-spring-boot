f87fb3924c85be676eda98a6e15663cf3c065d62
Shivakant6387 <shivakant5875@gmail.com>
2023-06-26 14:12:25 +0530

    modify application properties file

---
+++ src/main/java/com/example/shiva/youtubeclonespringboot/YouTubeCloneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiva.youtubeclonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeCloneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeCloneSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shiva/youtubeclonespringboot/YouTubeCloneSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiva.youtubeclonespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeCloneSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
